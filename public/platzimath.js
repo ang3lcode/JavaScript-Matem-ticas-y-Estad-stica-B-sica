4ead4e940b06e1f22711e08b19f8bb40b552800c
Luis Angel Perez Melendez <angelmelen88@gmail.com>
2022-09-17 18:26:53 -0500

    es par o impar

--- public/platzimath.js
+++ public/platzimath.js
@@ -1,3 +1,26 @@
+function esPar(lista) {
+    return !(lista.length % 2);
+}
+function esImpar(lista) {
+    return lista.length % 2;
+}
+  
+function calcularMediana(lista) {
+    const listaEsPar = esPar(lista);
+  
+    if (listaEsPar) {
+      // ...
+    } else {
+      const indexMitadListaImpar = Math.floor(lista.length / 2);
+      const medianaListaImpar = lista[indexMitadListaImpar];
+      console.log(indexMitadListaImpar);
+      console.log(medianaListaImpar);
+      return medianaListaImpar;
+    }
+}
+
+
+
 function calcularPromedio(lista) {
     // let sumaLista = 0;
     // for (let i = 0; i < lista.length; i++) {
