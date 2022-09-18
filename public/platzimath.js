534dd20066aa30683a9977ea86e93404844c8300
Luis Angel Perez Melendez <angelmelen88@gmail.com>
2022-09-17 20:01:45 -0500

    lista par, mediana

--- public/platzimath.js
+++ public/platzimath.js
@@ -9,7 +9,22 @@ function calcularMediana(lista) {
     const listaEsPar = esPar(lista);
   
     if (listaEsPar) {
-      // ...
+        const indexMitad1ListaPar = (lista.length / 2) - 1;
+        const indexMitad2ListaPar = lista.length / 2;
+        // const mitad1ListaPar = lista[(lista.length / 2) - 1];
+        // const mitad2ListaPar = lista[lista.length / 2];
+    
+        // lista[indexMitad1ListaPar] 
+        // lista[indexMitad2ListaPar] 
+        // [lista[indexMitad1ListaPar], lista[indexMitad2ListaPar]]
+        // const listaMitades = [mitad1ListaPar, mitad2ListaPar];
+    
+        const listaMitades = [];
+        listaMitades.push(lista[indexMitad1ListaPar]);
+        listaMitades.push(lista[indexMitad2ListaPar]);
+    
+        const medianaListaPar = calcularPromedio(listaMitades);
+        return medianaListaPar;
     } else {
       const indexMitadListaImpar = Math.floor(lista.length / 2);
       const medianaListaImpar = lista[indexMitadListaImpar];
