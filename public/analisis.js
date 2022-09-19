a8e33a7936527110d9d180a5a2b608f3e5caa179
Luis Angel Perez Melendez <angelmelen88@gmail.com>
2022-09-18 19:07:52 -0500

    empresas

--- public/analisis.js
+++ public/analisis.js
@@ -22,3 +22,64 @@ function medianaPorPersona(nombrePersona) {
   // console.log(medianaSalarios);
   return medianaSalarios;
 }
+
+
+function proyeccionPorPersona(nombrePersona) {
+    const trabajos = encontrarPersona(nombrePersona).trabajos;
+
+    let porcentajesCrecimiento = [];
+
+    for (let i = 1; i < trabajos.length; i++) {
+        const salarioActual = trabajos[i].salario;
+        const salariPasado = trabajos[i-1].salario;
+        const crecimiento = salarioActual - salariPasado;
+        const porcentajeCrecimiento = crecimiento / salariPasado;
+        
+        porcentajesCrecimiento.push(porcentajeCrecimiento)
+    }
+    const medianaPorcentajesCrecimiento = PlatziMath.calcularMediana(porcentajesCrecimiento);
+
+  // console.log({porcentajesCrecimiento, medianaPorcentajesCrecimiento});
+
+    const ultimoSalario = trabajos[trabajos.length - 1].salario;
+    const aumento = ultimoSalario * medianaPorcentajesCrecimiento;
+    const nuevoSalario = ultimoSalario + aumento;
+
+    return nuevoSalario;
+}
+
+//analisis empresarial
+
+// Análisis empresarial
+/* {
+  Industrias Mokepon: {
+    2018: [salario]
+  }
+  Industrias Mokepon: {
+    2018: [salario, salarios, salrios]
+    2019: 
+    2025: 
+    2026: 
+  },
+  Industrias Mokepon: {},
+  Industrias Mokepon: {},
+  Industrias Mokepon: {},
+} */
+
+const empresas = {};
+
+for (persona of salarios) {
+    for (trabajo of persona.trabajos) {
+        if (!empresas[trabajo.empresa]) {
+            empresas[trabajo.empresa] = {};
+        }
+        if (!empresas[trabajo.empresa][trabajo.year]) {
+            empresas[trabajo.empresa][trabajo.year] = [];
+        }
+        empresas[trabajo.empresa][trabajo.year].push(trabajo.salario);
+    }
+    
+}
+console.log({empresas})
+
+
